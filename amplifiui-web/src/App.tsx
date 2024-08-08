03a0942525fa1f73dccbf88663b7ae40269109e0
rhanierymueller <rhanimueller@hotmail.com>
2024-08-08 09:30:44 -0300

    some changes

--- amplifiui-web/src/App.tsx
+++ amplifiui-web/src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-
 function App() {
   return (
     <div className="text-center">
       <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-40 w-40 animate-spin-slow" alt="logo" />
         <p className="text-2xl">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -15,7 +11,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test
         </a>
       </header>
     </div>
