bccf851d044c1fc7a4d1b520644dab1cea8b824a
rhanierymueller <rhanimueller@hotmail.com>
2024-08-11 22:37:23 -0300

    Button Component

--- amplifiui-web/src/App.tsx
+++ amplifiui-web/src/App.tsx
@@ -1,21 +1,10 @@
-function App() {
-  return (
-    <div className="text-center">
-      <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
-        <p className="text-2xl">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-500 hover:text-blue-300"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import ButtonShowcase from './show-cases/ButtonsShowCases';
+
+const App: React.FC = () => (
+  <div>
+    <ButtonShowcase />
+  </div>
+);
 
 export default App;
